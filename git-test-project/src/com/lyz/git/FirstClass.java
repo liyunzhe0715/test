19b513da45a20b856a11874581e237dcbe9bfacb
liyunzhe <lyz_931402@163.com>
2020-09-15 18:34:39 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/lyz/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.lyz.git;
+
+import java.util.Arrays;
+
+/**
+ * git测试类
+ *
+ * @author liyunzhe
+ * @create 2020-09-15
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
